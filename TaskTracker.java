cd62e1682e6ef9df2ad807606cab2dcd0ce807a1
Daniel Merrett <daniel.merrett04@gmail.com>
2024-09-29 12:17:20 +0100

    Create TaskTracker skeleton
    Logic still needs to be implemented for all methods of both TaskTracker
    and Task classes. This includes the main method where the input and
    output will be managed and program flow will be controlled.

--- TaskTracker.java
+++ TaskTracker.java
@@ -1,4 +1,50 @@
 public class TaskTracker {
+    private Task[] tasks = new Task[50];
     public static void main (String[] args) {
+         // TODO: Implement logic for input and output
+    }
+
+    public Task[] getAllTasks() {
+        // TODO: Implement logic
+        return null;
+    }
+    
+    public Task[] getDoneTasks() {
+        // TODO: Implement logic
+        return null;
+    }
+
+    public Task[] getInProgressTasks() {
+         // TODO: Implement logic
+        return null;
+    }
+
+    public void markTaskDone(Task task) {
+         // TODO: Implement logic
+    }
+
+    public void addTaskFromString(String name) {
+         // TODO: Implement logic
+    }
+
+    public void updateTask(String name) {
+        // TODO: Implement logic
+    }
+
+    public void deleteTask(String name) {
+         // TODO: Implement logic
+    }
+
+    public Task getTaskFromName(String name) {
+         // TODO: Implement logic
+        return null;
+    }
+
+    public void updateFile() {
+         // TODO: Implement logic
+    }
+
+    public void createFile() {
+         // TODO: Implement logic
     }
 }
